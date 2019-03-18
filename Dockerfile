6701bb0882f5fee11fa11cb3ef07379ba3590421
Rui Lima <ruipedro.lima@gmail.com>
2019-03-18 02:53:47 +0000

    improved build

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM node:lts-jessie-slim
 
-RUN mkdir -p /srv/ghsearch-api
-WORKDIR /srv/ghsearch-api
-COPY dist/ ./
-
 ARG GITHUB_API_TOKEN
 ENV GITHUB_API_TOKEN $GITHUB_API_TOKEN
+ENV PORT 80
+
+COPY dist /srv/ghsearch-api
+WORKDIR /srv/ghsearch-api
 
 CMD ["node", "service.js"]
 
-EXPOSE 3000
+EXPOSE 80
--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 
 rm -rf dist/
 mkdir dist/
-cp -r {index,service}.js dist/
+cp {index,service}.js dist/
 cp package.json dist/
 cd dist/
 npm install --production
+cd ..
+
+$(aws ecr get-login --no-include-email --region eu-west-1)
+docker build -t ghsearch-api --build-arg GITHUB_API_TOKEN=$(cat .env) .
+docker tag ghsearch-api:latest 168268005725.dkr.ecr.eu-west-1.amazonaws.com/ghsearch-api:latest
+docker push 168268005725.dkr.ecr.eu-west-1.amazonaws.com/ghsearch-api:latest
