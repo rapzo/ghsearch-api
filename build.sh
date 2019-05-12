81c8541f7e5b876b9ab1e85613aa48f004a38570
Rui Lima <ruipedro.lima@gmail.com>
2019-05-12 15:15:57 +0100

    fighting thru the routers

--- build.sh
+++ build.sh
@@ -12,4 +12,6 @@ $(aws ecr get-login --no-include-email --region eu-west-1)
 docker build -t ghsearch-api --build-arg $(cat .env) .
 docker tag ghsearch-api:latest 168268005725.dkr.ecr.eu-west-1.amazonaws.com/ghsearch-api:latest
 docker push 168268005725.dkr.ecr.eu-west-1.amazonaws.com/ghsearch-api:latest
-aws ecs update-service --cluster api-cluster --service api-service --force-new-deployment
+# aws ecs update-service --cluster api-cluster --service api-service --force-new-deployment
+
+now -e $(cat .env)
